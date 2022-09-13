86a851a4749fe6ebf52383c1b6b16c4fd0935567
profmaojo <matthewojo8@gmail.com>
2022-09-13 15:16:15 -0700

    fun

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,17 @@
 /**
  * _isalpha - checks whether or not a character is an alphabet
  * @c: ascii value
- * return: 1 if it is lowercas or 0 otherwise
+ * Return: 1 if it is lowercase or 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	return ((c >= a && c <= z) || (c >= A && c = Z));
+	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
