d7b97c130be81074de977c3b3473c5983b209abb
profmaojo <matthewojo8@gmail.com>
2022-09-13 14:56:06 -0700

    functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,22 +5,19 @@
  * @n: positive, zero or negative integer
  * Return: 1 if it's positive, 0 if it's negative and -1 if it's negative
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	}
-	else if (n < 0)
-			{
-			_putchar('-');
-			return (-1);
-			}
-	else
+	} else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
 	}
+	return (-1);
 }
