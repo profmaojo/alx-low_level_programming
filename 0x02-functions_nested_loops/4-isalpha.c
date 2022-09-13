4bb61ed17e218e65fcefc6f2a43ad80aecea9952
profmaojo <matthewojo8@gmail.com>
2022-09-13 14:28:11 -0700

    fun

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,5 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 123) || (c >= 65 && c= 91))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((c >= 97 && c <= 123) || (c >= 65 && c= 91));
 }
