7e50e51ef7fdfe837161f7a54333833990e8f623
profmaojo <matthewojo8@gmail.com>
2022-09-28 12:39:28 -0700

    fun

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
