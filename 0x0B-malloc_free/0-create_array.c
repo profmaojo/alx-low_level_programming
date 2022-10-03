b66803d9fc28ccdcb6acd27415ac24a10ab1d119
profmaojo <matthewojo8@gmail.com>
2022-10-03 12:32:22 -0700

    fun

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,11 +3,10 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * create_array - Entry poin
+ * @size: size of the array
+ * @c: character
+ * Return: apointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
