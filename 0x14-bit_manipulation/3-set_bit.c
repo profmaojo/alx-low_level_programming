a74ccdf228718e6a56b00be93df05cd72b57d8ae
profmaojo <matthewojo8@gmail.com>
2022-10-27 12:02:26 -0700

    fun

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,8 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 53 || !n);
-	return (-1);
+	if (index > 53 || !n)
+		return (-1);
 	(*n |= 1 << index);
 	return (1);
-}
+g
